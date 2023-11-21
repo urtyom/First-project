4b8f680d4463b37e14a761dd90fbdb3c20901561
urtyom <urtyom.s@yandex.ru>
2023-11-21 15:09:43 +0300

    2.1.1

--- 2.1-databases/work_with_database/main/settings.py
+++ 2.1-databases/work_with_database/main/settings.py
@@ -51,11 +51,11 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wh',
+        'NAME': 'логин',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': '6757546',
+        'PASSWORD': 'пароль',
     }
 }
 
