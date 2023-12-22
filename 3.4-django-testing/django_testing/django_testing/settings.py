550bfa73173d3db6b8f6d979625f79081f19eeee
urtyom <dianalokk01@yandex.ru>
2023-12-22 18:05:40 +0300

    3.4.1

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -80,9 +80,11 @@ WSGI_APPLICATION = 'django_testing.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_django_testing',
+        'NAME': 'имя',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'пароль'
     }
 }
 
@@ -124,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,37 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from rest_framework.test import APIClient
+from model_bakery import baker
+
+from students.models import Course, Student
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs, make_m2m=True)
+
+    return factory()
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs, make_m2m=True)
+
+    return factory()
+
+
+@pytest.mark.django_db
+def test_api_get_first_course(client, course_factory):
+    courses = course_factory(_quantity=1)
+
+    response = client.get('/courses/')
+
+    data = response.json()
+    assert data['name'] == courses.name
+
