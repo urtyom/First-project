89e9712cc100ab388037ed8115eaed2e75587718
urtyom <dianalokk01@yandex.ru>
2023-12-29 08:15:25 +0300

    3.4.3

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -29,19 +29,19 @@ def student_factory():
 @pytest.mark.django_db
 def test_api_get_first_course(client, course_factory):
     courses = course_factory(_quantity=1)
+    course_id = courses[0].id
 
-    response = client.get('/courses/')
+    response = client.get(f'/api/v1/courses/{course_id}/')
 
     data = response.json()
-    for i, m in enumerate(data):
-        assert m['name'] == courses[i].name
+    assert data['name'] == courses[0].name
 
 
 @pytest.mark.django_db
 def test_api_get_list_courses(client, course_factory):
     courses = course_factory(_quantity=10)
 
-    response = client.get('/courses/')
+    response = client.get('/api/v1/courses/')
 
     data = response.json()
     assert type(data) == type(courses) == list
@@ -51,13 +51,18 @@ def test_api_get_list_courses(client, course_factory):
 def test_api_get_filter_id_courses(client, course_factory):
     courses = course_factory(_quantity=10)
 
-    response = client.get('/courses/')
+    response = client.get('/api/v1/courses/')
 
     data = response.json()
     random_course = data[0]
     course_id = random_course['id']
 
-    response = client.get('/courses/?id='+str(course_id))
+    filters = {
+        'id': course_id
+    }
+
+    url = f'/api/v1/courses/'
+    response = client.get(url, data=filters)
 
     filtered_courses = response.data
 
@@ -69,21 +74,23 @@ def test_api_get_filter_id_courses(client, course_factory):
 def test_api_get_filter_name_courses(client, course_factory):
     courses = course_factory(_quantity=10)
 
-    response = client.get('/courses/')
+    response = client.get('/api/v1/courses/')
 
     data = response.json()
     random_course = data[0]
     course_name = random_course['name']
 
-    response = client.get('/courses/?name='+str(course_name))
+    filters = {
+        'name': course_name
+    }
+
+    url = f'/courses/'
+    response = client.get(url, data=filters)
 
     filtered_courses = response.data
 
     assert len(filtered_courses) == 1
     assert filtered_courses[0]['name'] == course_name
-
-
-from django.urls import reverse
 
 
 @pytest.mark.django_db
@@ -95,7 +102,7 @@ def test_api_update_courses(client, course_factory):
         'name': "Новое название курса",
     }
 
-    url = f'/courses/{course_id}/'
+    url = f'/api/v1/courses/{course_id}/'
     response = client.patch(url, data=update_data)
 
     assert response.status_code == 200
@@ -110,7 +117,7 @@ def test_api_delete_course(client, course_factory):
     courses = course_factory(_quantity=1)
     course_id = courses[0].id
 
-    url = f'/courses/{course_id}/'
+    url = f'/api/v1/courses/{course_id}/'
     response = client.delete(url)
 
     assert response.status_code == 204
